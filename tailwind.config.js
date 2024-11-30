fd1c0bf8d4d18ad8e7608d46b4a85c6a5785674e
adityaspande1 <121101849+adityaspande1@users.noreply.github.com>
2024-11-30 21:02:41 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Ensure all components are scanned
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "#e6f7ff",
+        primary: "#005f99", 
+        accent: "#00aaff", 
+      },
+    },
+  },
+  plugins: [],
+};
